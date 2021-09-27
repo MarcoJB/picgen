6e69379f99cfa12401cb46b11e5761b0464d6a70
Tonda93 <marco.batschkowski@gmail.com>
2021-09-27 22:39:16 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'picgen2';
+  title = 'ZERO WASTE';
+  subtitle = 'Unverpackt-Foodcoop Karlsruhe';
+  bgImage = 'https://www.unverpackt-foodcoop.de/images/IMG_4778.png'
+
+  save() {
+    // @ts-ignore
+    html2canvas(document.querySelector("#mainImage")).then(canvas => {
+      const link = document.createElement("a")
+      link.setAttribute('download', 'picgen.png');
+      link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png",
+        "image/octet-stream"));
+      link.click();
+    });
+  }
 }
