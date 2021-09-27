268409e967f1c8b234bbd6b5ff8aacc9ddc8192f
Tonda93 <marco.batschkowski@gmail.com>
2021-09-27 22:50:08 +0200

    Rename to picgen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'picgen2'`, () => {
+  it(`should have as title 'picgen'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('picgen2');
+    expect(app.title).toEqual('picgen');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('picgen2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('picgen app is running!');
   });
 });
