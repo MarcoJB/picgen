f5a8ecc9464616c9f0ec67a94b5a508e6f0a7c57
Tonda93 <marco.batschkowski@gmail.com>
2021-09-29 22:21:30 +0200

    implement copyright text, logo-color, background-image (+ position)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import html2canvas from "html2canvas";
 
 @Component({
@@ -7,9 +7,30 @@ import html2canvas from "html2canvas";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ZERO WASTE';
-  subtitle = 'Unverpackt-Foodcoop Karlsruhe';
+  title = 'Überschrift';
+  subtitle = 'Unterüberschrift';
+  copyright = '© Greenpeace Karlsruhe'
+  logoColor = "logo_gpka_gw"
   saving = false;
+  scaleFactor = 1;
+  mainImageSource = "";
+  @ViewChild('mainImage')
+  mainImage!: ElementRef<HTMLImageElement>;
+  mainImageHorizontalPositioning = "RIGHT";
+  mainImageVerticalPositioning = "CENTER";
+
+  ngOnInit() {
+    this.calcScaleFactor();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(even:any) {
+    this.calcScaleFactor()
+  }
+
+  calcScaleFactor() {
+    this.scaleFactor = Math.min(1200, window.innerHeight*0.9) / 1200;
+  }
 
   save() {
     this.saving = true;
@@ -24,10 +45,50 @@ export class AppComponent {
     // @ts-ignore
     html2canvas(document.querySelector("#previewInstagram")).then(canvas => {
       const link = document.createElement("a")
-      link.setAttribute('download', 'picgen.png');
+      link.setAttribute('download', 'picgen.png')
       link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png",
-        "image/octet-stream"));
-      link.click();
+        "image/octet-stream"))
+      link.click()
     });
+  }
+
+  loadImage(event: Event) {
+    // @ts-ignore
+    const file = event.target.files[0]
+    const reader = new FileReader()
+
+
+    reader.addEventListener("load", () => {
+      if (reader.result !== null) {
+        this.mainImage.nativeElement.src = reader.result.toString()
+        setTimeout(() => this.resetImage())
+      }
+    }, false);
+
+    if (file) {
+      reader.readAsDataURL(file)
+    }
+  }
+
+  resetImage() {
+    const ratio = this.mainImage.nativeElement.naturalWidth / this.mainImage.nativeElement.naturalHeight
+
+    if (ratio > 1) {
+      this.mainImage.nativeElement.style.height = "1200px"
+      this.mainImage.nativeElement.style.top = "0"
+
+      switch (this.mainImageHorizontalPositioning) {
+        case "LEFT":
+          this.mainImage.nativeElement.style.left = "0"
+          break
+        case "CENTER":
+          this.mainImage.nativeElement.style.left = "-" + (ratio-1)*600 + "px"
+          break
+        case "RIGHT":
+          this.mainImage.nativeElement.style.left = "-" + (ratio-1)*1200 + "px"
+          break
+      }
+
+    }
   }
 }
