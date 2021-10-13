b208382b34e2de0cf3828ebfcd4d1f6719a6b3bd
Tonda93 <marco.batschkowski@gmail.com>
2021-10-13 23:17:40 +0200

    Update design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { EditorComponent } from './editor/editor.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
     imports: [
-        BrowserModule,
-        FormsModule
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatSelectModule,
+      MatInputModule,
+      MatDividerModule,
+      MatButtonToggleModule,
+      MatSlideToggleModule,
+      MatIconModule,
+      MatButtonModule,
+      MatProgressBarModule,
+      MatProgressSpinnerModule
     ],
   providers: [],
   bootstrap: [AppComponent]
